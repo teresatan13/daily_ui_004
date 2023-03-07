9501bf19ce11730d5e376e552e79dd7e349a62c8
Teresa <ttan@gmail.com>
2023-03-07 14:12:33 -0800

    revised

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function clearScreen() {
+    document.getElementById("result").value = '';
+}
+
+
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
+
+function calculate() {
+
+}
